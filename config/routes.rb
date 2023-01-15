d74d22568568081c07b8018a8c88decd274bd193
Sho Abe <shoabe00@gmail.com>
2023-01-16 01:06:39 +0900

    add active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  get 'gears/index'
+  get 'gears/show/:id', to: 'gears#show'
+  get 'gears/new', as: 'gears_new'
+  post 'gears', to: 'gears#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
